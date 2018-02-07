0765a75b187aef44c940de0752256fbcf0f5e929
peichao <peichao.wang@bitland.corp-partner.google.com>
2018-02-07 15:54:54 +0800

    add vim

--- bin/github_copy.sh
+++ bin/github_copy.sh
@@ -2,8 +2,15 @@
 
 cd ~/proj/github
 
+#... bin
 cp -rvf ~/bin ~/proj/github
+
+#... vim
+cp -rvf ~/.vimrc.bundles.local ~/proj/github/vim
+cp -rvf ~/.vimrc.local ~/proj/github/vim
 
 git add .
 git commit
 git push origin master -f
+
+cd -
